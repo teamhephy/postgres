dd9c2da2ab06669f752f7e856a26cb388fbec6d4
duanhongyi <duanhongyi@doopai.com>
2018-12-01 22:57:30 +0800

    feat(workflow): add parameters to determine that s3 enabled SSE

--- rootfs/patcher-script.d/patch_wal_e_s3.py
+++ rootfs/patcher-script.d/patch_wal_e_s3.py
@@ -7,10 +7,12 @@ def patch_uri_put_file():
         k = s3_util._uri_to_key(creds, uri, conn=conn)
         if content_type is not None:
             k.content_type = content_type
+
+        # Currently WALE only supports AES256, so it's a boolean value.
+        encrypt_key = False
         if os.getenv('DATABASE_STORAGE') == 's3':
-            encrypt_key=True
-        else:
-            encrypt_key=False
+            if os.getenv('WALE_S3_SSE', 'None') == 'AES256':
+                encrypt_key = True
         k.set_contents_from_file(fp, encrypt_key=encrypt_key)
         return k
     s3.uri_put_file = wrap_uri_put_file
